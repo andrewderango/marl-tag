fe7716032971ef97074832b0cb3c145795fb038c
Hashim Bukhtiar <hashbukhtiar@gmail.com>
2026-03-21 17:37:53 -0400

    fixed eval and figure generation post-training

--- figures/plot_results.py
+++ figures/plot_results.py
@@ -23,6 +23,8 @@ import argparse
 import os
 import sys
 
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import numpy as np
 import pandas as pd
 import matplotlib
